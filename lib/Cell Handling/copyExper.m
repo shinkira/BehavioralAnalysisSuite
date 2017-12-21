a3c8a8be6b6eee42809c1f506563d4b175361503
tedlutkus <theodore_lutkus@hms.harvard.edu>
2017-12-21 11:18:51 -0500

    Turn off warning when loading objects

--- lib/Cell Handling/copyExper.m
+++ lib/Cell Handling/copyExper.m
@@ -9,7 +9,9 @@ function [exper] = copyExper(path,tempName)
 %load experiment file
 fileName = [path,tempName,'.mat'];
 if exist(fileName) ~= 0
+    warning('off') %#ok<WNOFF>
     load(fileName,'exper');
+    warning('on') %#ok<WNON>
 else
     exper = 0;
 end
