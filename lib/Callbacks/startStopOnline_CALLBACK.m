e344d390d53a57b3bcff828d9265b099f505c1a0
ShinOnCoopPC <shinkira3@gmail.com>
2017-12-26 12:49:28 -0500

    Revert "startStopOline_CALLBACK udated to automatically rerun Live Aq after each session"
    This reverts commit 4007447a6795550de6c14eecf53063fb4a9503ce.

--- lib/Callbacks/startStopOnline_CALLBACK.m
+++ lib/Callbacks/startStopOnline_CALLBACK.m
@@ -10,9 +10,6 @@ tempCellPath = [tempPath,'\',guiObjects.userData.tempName,'Cell.mat'];
 %get update interval
 updateInt = str2double(get(guiObjects.updateInt,'String'));
 
-rep = 0;
-max_rep = 60;
-
 if isfield(guiObjects,'onlineTimer') %if timer exists, stop it and delete
     if isvalid(guiObjects.onlineTimer)
         stop(guiObjects.onlineTimer); %stop the timer
@@ -29,32 +26,19 @@ if isfield(guiObjects,'onlineTimer') %if timer exists, stop it and delete
     end
 else %otherwise create it! 
     %create timer
-    while 1
-        try
-            rep = rep+1;
-            
-            guiObjects.onlineTimer = timer;
-            set(guiObjects.onlineTimer,'ExecutionMode','fixedSpacing','TimerFcn',...
-                {@openCellFiles_CALLBACK,guiObjects,true,tempCellPath,true},...
-                'Period',updateInt,'Name','Online Timer');
-
-            %save guiObjects
-            set(guiObjects.figHandle,'UserData',guiObjects);
-
-            %change text on button
-            set(guiObjects.startStop,'String','Stop Live Aq');
-
-            %start timer
-            start(guiObjects.onlineTimer);
-            
-        catch
-            fprintf('Error occured while running Live Aq (%d / %d)\nRetry in 30sec\n',rep,60)
-            pause(30)
-            if rep > max_rep
-                break
-            end
-        end
-    end
+    guiObjects.onlineTimer = timer;
+    set(guiObjects.onlineTimer,'ExecutionMode','fixedSpacing','TimerFcn',...
+        {@openCellFiles_CALLBACK,guiObjects,true,tempCellPath,true},...
+        'Period',updateInt,'Name','Online Timer');
+    
+    %save guiObjects
+    set(guiObjects.figHandle,'UserData',guiObjects);
+    
+    %change text on button
+    set(guiObjects.startStop,'String','Stop Live Aq');
+    
+    %start timer
+    start(guiObjects.onlineTimer);
 end
 
 %save guiObjects
