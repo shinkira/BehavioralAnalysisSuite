7f8ffe0ff1c2bb9a31a4c570e31a8b4c5271e2ca
ShinOnCoopPC <shinkira3@gmail.com>
2018-02-13 17:30:52 -0500

    Parsing through dir method

--- lib/Daily Mouse Review/pastDaysBe.m
+++ lib/Daily Mouse Review/pastDaysBe.m
@@ -10,9 +10,43 @@ initials = getInitials(mouseN);
 
 %Finding the mouse folder
 mouse = strcat('\\research.files.med.harvard.edu\Neurobio\HarveyLab\Tier1\Shin\ShinDataAll\BASfigs\',initials,'0',mouseNum);
+picList = dir(mouse);
+
+function latestfile = getlatestfile(directory)
+%This function returns the latest file from the directory passsed as input
+%argument
+
+%Get the directory contents
+dirc = dir(directory);
+
+%Filter out all the folders.
+dirc = dirc(find(~cellfun(@isdir,{dirc(:).name})));
+dirc_p = dirc(find(any(regexp(dirc(:).name,'.png$'))));
+
+%I contains the index to the biggest number which is the latest file
+[A,I] = max([dirc_p(:).datenum]);
+
+if ~isempty(I)
+    latestfile = dirc_p(I).name;
+end
+
+end
 
 oldFolder = cd(mouse);
 
+k = 1;
+for i=1:7
+    if k>4
+        break
+    end
+    img = getlatestfile(mouse);
+    bas{k,1} = imread(img);
+    basCrop{k,1} = imcrop(bas{i,1},[0.5 0.5 1328 579]);
+    k = k+1;
+end
+
+
+%{
 %Getting the past four days in a variable
 day = datetime('today');
 pastDayTimes = dateshift(day,'start','day',-6:0);
@@ -25,22 +59,7 @@ pastDayTimes = dateshift(day,'start','day',-6:0);
 check = 0;
 existCount = 0;
 while check == 0
-    for i=7:-1:2
-        if isweekend(pastDayTimes(i)) == 1
-            pastDayTimes(i:i-1:i+1) = dateshift(pastDayTimes(i:i-1:i+1),'dayofweek','Friday','previous');
-            i=1;
-        elseif pastDayTimes(i) == pastDayTimes(i-1)
-            pastDayTimes(i-1:-1:2) = dateshift(pastDayTimes(i-1:-1:2),'start','day',-1);
-            i=1;
-            if pastDayTimes(1) >= pastDayTimes(2)
-                pastDayTimes(1) = dateshift(pastDayTimes(1),'start','day',-1);
-                if isweekend(pastDayTimes(1))
-                    pastDayTimes(1) = dateshift(pastDayTimes(1),'dayofweek','Friday','previous');
-                end
-            end
-        end
-    end
-
+ 
     %Cleaning up the pastDays variable to access the .png images
     pastDays = pastDayTimes(1:7)';
     pastDays = datestr(pastDays,'yymmdd');
@@ -60,6 +79,23 @@ while check == 0
     end
 end
 
+%{
+  for i=7:-1:2
+        if isweekend(pastDayTimes(i)) == 1
+            pastDayTimes(i:i-1:i+1) = dateshift(pastDayTimes(i:i-1:i+1),'dayofweek','Friday','previous');
+            i=1;
+        elseif pastDayTimes(i) == pastDayTimes(i-1)
+            pastDayTimes(i-1:-1:2) = dateshift(pastDayTimes(i-1:-1:2),'start','day',-1);
+            i=1;
+            if pastDayTimes(1) >= pastDayTimes(2)
+                pastDayTimes(1) = dateshift(pastDayTimes(1),'start','day',-1);
+                if isweekend(pastDayTimes(1))
+                    pastDayTimes(1) = dateshift(pastDayTimes(1),'dayofweek','Friday','previous');
+                end
+            end
+        end
+    end
+%}
 pastDays = flip(pastDays);
 
 %Looping over each image and cropping for figure
@@ -74,6 +110,7 @@ for i=1:7
         k = k+1;        
     end
 end
+%}
 
 %Returning to original directory location
 cd(oldFolder)
