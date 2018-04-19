51ad464878645038348654267b5488cd7e415902
Joe <deluisi.j@husky.neu.edu>
2018-04-19 15:16:01 -0400

    figures for JD65 and JD66

--- lib/Daily Mouse Review/dailyBASfigs.m
+++ lib/Daily Mouse Review/dailyBASfigs.m
@@ -2,7 +2,7 @@ function dailyBASfigs(varargin)
 
 % default params
 userFile = 'userData_SK_Rig_5_ViRMEn.txt';
-mouse_list = 47:64;
+mouse_list = 47:66;
 % mydate = datestr(now,'yymmdd');
 % mydate = num2str(str2double(mydate)-1);
 
