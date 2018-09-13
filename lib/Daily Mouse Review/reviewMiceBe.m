bca6d63c1309b6f4fc7a8579c20ed6e72010a711
Alec <nelson.ale@husky.neu.edu>
2018-09-13 15:08:43 -0400

    Clean up current mouse list

--- lib/Daily Mouse Review/reviewMiceBe.m
+++ lib/Daily Mouse Review/reviewMiceBe.m
@@ -145,7 +145,7 @@ function mouseNumPopup_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
-set(hObject,'String',{'47';'51';'55';'58';'59';'60';'62';'63';'64';'65';'66';'67';'68';'69';'70';'71';'72';'74'});
+set(hObject,'String',{'47';'51';'59';'60';'68';'69';'70';'71';'72';'73';'74'});
 
 
 % --- Executes when mainFigure is resized.
