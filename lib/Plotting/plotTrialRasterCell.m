28fc8c8d60800f9baee59926b20de69eec85f6b1
Shin Kira <shin_kira@hms.harvard.edu>
2025-11-27 15:40:39 -0500

    Update plotTrialRasterCell.m

--- lib/Plotting/plotTrialRasterCell.m
+++ lib/Plotting/plotTrialRasterCell.m
@@ -104,7 +104,7 @@ for i=1:size(dataCell,2)
     set(guiObjects.rasterHandle(i),'ButtonDownFcn',{@rasterClick_CALLBACK,...
         dataCell,i});
     rasterLocation(i) = ind/totTime;
-    set(guiObjects.rasterHandle(i),'Color',cmapCustom(dataCell{i}.maze.condition,:),'LineWidth',3);
+    set(guiObjects.rasterHandle(i),'Color',cmapCustom(dataCell{i}.maze.condition+1,:),'LineWidth',3);
 end
 xlim([0 1]);
 xTickVals = num2cell(timeVec(round(linspace(1,length(timeVec),11))));
